bd4808a6939913c10bc258e4274279b1d9e9980c
Kezia Campos <yourGitHub@email.com>
2020-12-21 18:10:29 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,123 +1,205 @@
 <!DOCTYPE html>
-<!-- saved from url=(0053)https://getbootstrap.com/docs/4.5/examples/dashboard/ -->
+<!-- saved from url=(0056)https://getbootstrap.com.br/docs/4.1/examples/dashboard/ -->
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v4.1.1">
-    <title>Dashboard Template · Bootstrap</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/dashboard/">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="https://getbootstrap.com.br/favicon.ico">
+  <script src="https://kit.fontawesome.com/8786c39b09.js" crossorigin="anonymous"></script>
+  <title>Empresa</title>
 
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
-<link rel="icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/favicon.ico">
-<meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
-<meta name="theme-color" content="#563d7c">
-   
-        
+
+
+
+</style>
+
+<style type="text/css">
+  @media only screen and (max-width: 600px) {
+    body {
+      width : 350px;
+      text-align: center;
+    }
+  }
+
+
+</style>
 </head>
-  <body>
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Empresa</a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <ul class="navbar-nav px-3">
-    <li class="nav-item text-nowrap">
-      <a class="nav-link" href="#">Sair</a>
-    </li>
-  </ul>
-</nav>
 
-<div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="sidebar-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="?pagina">
-              Dashboard <span class="sr-only">(atual)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="?pagina=pedidos"> 
-              Pedidos
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="?pagina=clientes">
-              Cadastrar Clientes
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="?pagina=vendas">
-              Cadastrar Vendas
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
 
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group mr-2">
-           
+<body>
+  <nav class="navbar navbar-expand-lg d-none d-sm-block" style="background-color: #db6400; color: #f8f1f1">
+    <a class="navbar-brand" href="#" style="text-decoration: none;color: #fff"><img src="https://user-images.githubusercontent.com/1256531/43319337-f5ea7a92-9169-11e8-9622-a3c7f0a0e5c4.png" width="50px" height="50px"> Dashboard</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="container" style="margin-top: -50px">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="text-decoration: none;color: #fff">
+            Empresa
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Ação</a>
+            <a class="dropdown-item" href="#">Outra ação</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Algo mais aqui</a>
           </div>
-          
+        </li>
+
+      </ul>
+    </div>
+  </div>
+  </nav>
+
+  <div class="container-fluid" >
+    <div class="row">
+      <nav class="col-md-2 d-none d-md-block sidebar" style='background-color:#fff;border-right: 1px solid #f3f3f3'>
+        <div class="sidebar-sticky">
+
+          <ul class="nav flex-column"  style="padding-top: 20px">
+            <li class="nav-item">
+              <a class="nav-link active" href="?pagina" style="color:#000;text-decoration: none">
+
+                <i class="fas fa-tachometer-alt"></i>&nbsp;Dashboard <span class="sr-only">(atual)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?pagina=pedidos" style="color:#000;text-decoration: none"> 
+
+                <i class="fas fa-box"></i>&nbsp;Pedidos
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?pagina=clientes" style="color:#000;text-decoration: none">
+
+                <i class="far fa-user"></i>&nbsp;Cadastrar Clientes
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?pagina=vendas" style="color:#000;text-decoration: none">
+
+                <i class="fas fa-dollar-sign"></i>&nbsp;Cadastrar Vendas
+              </a>
+            </li>
+            
+          </ul>
+
+
         </div>
 
-<!-- Aqui entra o php do menu do dashboard--> 
 
-      </div>
-      <?php
-       if(isset($_GET['pagina']))
-          {
-            switch ($_GET['pagina'])
-            {
-              case 'pedidos':
-                echo '<h2>Pedidos</h2>';
-                include 'graficos/graficoArea.php';
-              break;
+        
+      </nav>
 
-              case 'clientes':
-                echo '<h2>Clientes</h2>';
-                include 'cadastro_cliente.php';
-              break;
+
+
+
+
+
+      <!-- conteudo -->
+      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 class="h2">Dashboard</h1>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group mr-2">
+
+            </div>
+
+          </div>
+        </div>
+        <?php
+
+        if(isset($_GET['pagina'])) {
+
+          switch ($_GET['pagina']) {
+            case 'pedidos':
+            echo '<h2>Pedidos</h2>';
+            include 'graficos/graficoArea.php';
+            break;
+
+            case 'clientes':
+            include 'cadastro_cliente.php';
+            break;
 
             case 'vendas':
-              echo '<h2>Cadastro de vendas</h2>';
-              include 'cadastro_vendas.php';
+
+            include 'cadastro_vendas.php';
             break;
 
             default:
-              include "painel.php";
-              include "painel2.php";
-            break;
-            }
+            include 'painel.php'; include 'painel2.php'; 
 
+            break;
           }
 
-      ?>
+
+
+        }
 
 
 
-    </main>
+        ?>
+
+
+      </main>
+    </div>
   </div>
-</div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+  <div class="d-block d-sm-none">
+    <nav class="navbar fixed-bottom navbar-light bg-light">
+      <a class="navbar-brand" href="#">Empresa</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+        <ul class="navbar-nav mr-auto">
+          <a class="nav-link active" href="?pagina">
 
-</body>
-</html>
+            Dashboard <span class="sr-only">(atual)</span>
+          </a>
+        </li>
+        <li class="nav-item" >
+          <a class="nav-link" href="?pagina=pedidos" style="text-decoration: none"> 
+
+            Pedidos
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?pagina=clientes">
+
+            Cadastrar Clientes
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?pagina=vendas">
+
+            Cadastrar Vendas
+          </a>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
+</div> 
+
+    <!-- Principal JavaScript do Bootstrap
+      ================================================== -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+      <script type="text/javascript">
+
+        $(window).resize(function(){
+          drawChart();
+          drawChart2();
+        });
+
+      </script>
+
+    </body>
+    </html>
